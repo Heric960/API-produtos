3a8e4f3e7935c2cdc28366819c475c102c663e42
Heric960 <hericalves960@gmail.com>
2025-03-31 21:02:56 -0300

    Criando o projeto

---
+++ produtosapi/src/test/java/com/heric/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heric.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
