13e848a0826512df36c7f53d3a1553343ed02df5
Heric960 <hericalves960@gmail.com>
2025-04-02 19:20:14 -0300

    fializando a classe produto

--- produtosapi/src/main/java/com/heric/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/heric/produtosapi/controller/ProdutoController.java
@@ -1,19 +1,44 @@
 package com.heric.produtosapi.controller;
 
 import com.heric.produtosapi.model.Produto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.heric.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto recebido: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
         return produto;
     }
 
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id" ) String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualiza(@PathVariable("id") String id, @RequestBody Produto produto) {
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
 }
